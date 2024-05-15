5228cea1cc665de31a7ce0648a948d2c15153371
Krishi Shah <krishishah1211@gmail.com>
2024-05-15 23:45:46 +0530

    added company

--- src/constants.js
+++ src/constants.js
@@ -1,22 +1,59 @@
-import { HTML, CSS, JavaScript, React, Redux, SASS, Bootstrap, TailwindCSS, MaterialUI, NodeJS, ExpressJS, MongoDB, MySQL, Firebase, C, CPP, Java, Python, Git, Github, VSCode, Postman, Figma } from './assets/export';
-import { Webexcep, GirlScript, Hack2Skill } from './assets/export';
-import { OKIMR, Excellence, Alpha } from './assets/export';
-import { HooBank, Gpt3, Anon, Hotstar, GardenCafe, Fithub } from './assets/export';
-
+import {
+  HTML,
+  CSS,
+  JavaScript,
+  React,
+  Redux,
+  SASS,
+  Bootstrap,
+  TailwindCSS,
+  MaterialUI,
+  NodeJS,
+  ExpressJS,
+  MongoDB,
+  MySQL,
+  Firebase,
+  C,
+  CPP,
+  Java,
+  Python,
+  Git,
+  Github,
+  VSCode,
+  Postman,
+  Figma,
+} from "./assets/export";
+import { Webexcep, GirlScript, Hack2Skill } from "./assets/export";
+import { OKIMR, Excellence, Alpha } from "./assets/export";
+import {
+  HooBank,
+  Gpt3,
+  Anon,
+  Hotstar,
+  GardenCafe,
+  Fithub,
+} from "./assets/export";
 
 export const Bio = {
   name: "Krishi Shah",
-  roles: ["Full-Stack Developer", "UI/UX Designer", "Freelancer","Python Developer"],
-  description: "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  resume: "https://drive.google.com/file/d/1Pfg7xdoIKj30NILihlXt_rl1Bx4XMemZ/view?usp=drive_link",
+  roles: [
+    "Full-Stack Developer",
+    "UI/UX Designer",
+    "Freelancer",
+    "Python Developer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  resume:
+    "https://drive.google.com/file/d/1Pfg7xdoIKj30NILihlXt_rl1Bx4XMemZ/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/krishi-shah-1n2ov/",
   github: "https://github.com/Krishi1211",
   leetcode: "https://leetcode.com/krishishah1211/",
   geeksforgeeks: "https://auth.geeksforgeeks.org/user/krishishwfbw",
   instagram: "https://www.instagram.com/krishi_shah_1211/",
-  whatsapp: "https://wa.me/8369386540?text=Hi Krishi! I'd like to know more about your services."
+  whatsapp:
+    "https://wa.me/8369386540?text=Hi Krishi! I'd like to know more about your services.",
 };
-
 
 export const skills = [
   {
@@ -30,8 +67,8 @@ export const skills = [
       { name: "SASS", image: SASS },
       { name: "Bootstrap", image: Bootstrap },
       { name: "Tailwind CSS", image: TailwindCSS },
-      { name: "Material UI", image: MaterialUI }
-    ]
+      { name: "Material UI", image: MaterialUI },
+    ],
   },
 
   {
@@ -41,8 +78,8 @@ export const skills = [
       { name: "Express JS", image: ExpressJS },
       { name: "MongoDB", image: MongoDB },
       { name: "MySQL", image: MySQL },
-      { name: "Firebase", image: Firebase }
-    ]
+      { name: "Firebase", image: Firebase },
+    ],
   },
 
   {
@@ -52,8 +89,8 @@ export const skills = [
       { name: "C++", image: CPP },
       { name: "Java", image: Java },
       { name: "JavaScript", image: JavaScript },
-      { name: "Python", image: Python }
-    ]
+      { name: "Python", image: Python },
+    ],
   },
 
   {
@@ -63,21 +100,38 @@ export const skills = [
       { name: "Github", image: Github },
       { name: "VS Code", image: VSCode },
       { name: "Postman", image: Postman },
-      { name: "Figma", image: Figma }
-    ]
-  }
+      { name: "Figma", image: Figma },
+    ],
+  },
 ];
-
 
 export const experiences = [
   {
     img: Webexcep,
     role: "Frontend Developer Intern",
-    company: "Webexcep",
-    date: "Sep 2023 - Present",
-    desc: "During my internship at Webexcep, I had the opportunity to gain hands-on experience in frontend development. I contributed to the development of web applications, collaborated with the development team, and worked on various projects to enhance my skills in web development.",
-    skills: ["HTML", "CSS", "JavaScript", "React JS", "SASS", "Bootstrap", "Tailwind CSS", "Material UI"],
-    doc: ""
+    company: "Kenmark Itan Solutions",
+    date: "August 2023 - Present",
+    desc: "During my internship at Kenmark, I had the opportunity to gain hands-on experience in frontend development. I contributed to the development of web applications using Next JS, collaborated with the development team, and worked on various projects to enhance my skills in web development.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React JS",
+      "Next JS",
+      "Bootstrap",
+      "Tailwind CSS",
+      "Material UI",
+    ],
+    doc: "",
+  },
+  {
+    img: Hack2Skill,
+    role: "Co‑Lead Fullstack Development",
+    company: "IEEE Bombay Section X Alhansat Technology ",
+    date: "September,2023‑November,2023",
+    desc: "I had the opportunity to contribute as a frontend developer in the open source community of Hack2Skill, a platform dedicated to advancing cybersecurity and ethical hacking through technology.",
+    skills: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+    doc: "",
   },
   {
     img: GirlScript,
@@ -86,19 +140,9 @@ export const experiences = [
     date: "May 2023 - Aug 2023",
     desc: "I had the privilege of participating in GirlScript Summer of Code, a prestigious open source program that encourages young talents to contribute to various open source projects.",
     skills: ["HTML", "CSS", "Bootstrap", "JavaScript"],
-    doc: ""
+    doc: "",
   },
-  {
-    img: Hack2Skill,
-    role: "Open Source Contributor",
-    company: "Hack2Skill",
-    date: "Jun 2023 - Jul 2023",
-    desc: "I had the opportunity to contribute as a frontend developer in the open source community of Hack2Skill, a platform dedicated to advancing cybersecurity and ethical hacking through technology.",
-    skills: ["HTML", "CSS", "Bootstrap", "JavaScript"],
-    doc: ""
-  }
 ];
-
 
 export const education = [
   {
@@ -107,7 +151,7 @@ export const education = [
     date: "Sep 2022 - Jun 2025",
     grade: "85.71%",
     desc: "I am currently pursuing a Bachelor's degree in Computer Application at Om Kothari Institute of Management & Research, Kota, where I am honing my skills in computer science and information technology. This program is equipping me with the knowledge and expertise required to thrive in the dynamic field of technology and software development.",
-    degree: "BCA (Bachelor in Computer Application)"
+    degree: "BCA (Bachelor in Computer Application)",
   },
   {
     img: Excellence,
@@ -115,7 +159,7 @@ export const education = [
     date: "Jul 2021 - Jun 2022",
     grade: "87.40%",
     desc: "I completed my 12th-grade education at Government Excellence School, Sheopur, with a strong focus on Physics, Chemistry, and Mathematics (PCM). This experience laid the foundation for my academic journey, equipping me with essential problem-solving skills and a passion for the sciences.",
-    degree: "12th - PCM"
+    degree: "12th - PCM",
   },
   {
     img: Alpha,
@@ -123,15 +167,15 @@ export const education = [
     date: "Jul 2019 - Jun 2020",
     grade: "90.33%",
     desc: "I completed my 10th-grade education at Alpha Higher Secondary School, where I acquired a solid educational foundation and developed valuable study habits that would serve me well in subsequent academic endeavors",
-    degree: "10th"
-  }
+    degree: "10th",
+  },
 ];
-
 
 export const projects = [
   {
     title: "Hoobank Website",
-    description: "Developed a Real-World Bank Website named 'Hoobank' that offers a Unique and Engaging Experience to their users. It Has 4 Sections - Home, Features, Products & Clients Section. 'Hoobank' is a fully responsive website developed using Vite JS and Tailwind CSS. Regardless of the device you use, be it a desktop computer, tabley, or smartphone, 'Hoobank' ensures a smooth browsing experience.",
+    description:
+      "Developed a Real-World Bank Website named 'Hoobank' that offers a Unique and Engaging Experience to their users. It Has 4 Sections - Home, Features, Products & Clients Section. 'Hoobank' is a fully responsive website developed using Vite JS and Tailwind CSS. Regardless of the device you use, be it a desktop computer, tabley, or smartphone, 'Hoobank' ensures a smooth browsing experience.",
     image: HooBank,
     tags: ["Vite JS", "Tailwind CSS"],
     github: "https://github.com/anshulgupta48/Hoobank-Website",
@@ -139,7 +183,8 @@ export const projects = [
   },
   {
     title: "GPT3-OpenAI Website",
-    description: "'GPT3-OpenAI' is a Real-World AI-Tool Website that offers you a collection of informative articles that dive deep into the world of AI. It Has 5 Sections - Home, What is GPT3, Features, Possibility & Blogs Section from Where is Created to Provide a Unique and Engaging experience. 'GPT3-OpenAI' is a fully responsive website developed using React JS and Sass. Regardless of the device you use, be it a desktop computer, tabley, or smartphone, 'GPT3-OpenAI' ensures a smooth browsing experience.",
+    description:
+      "'GPT3-OpenAI' is a Real-World AI-Tool Website that offers you a collection of informative articles that dive deep into the world of AI. It Has 5 Sections - Home, What is GPT3, Features, Possibility & Blogs Section from Where is Created to Provide a Unique and Engaging experience. 'GPT3-OpenAI' is a fully responsive website developed using React JS and Sass. Regardless of the device you use, be it a desktop computer, tabley, or smartphone, 'GPT3-OpenAI' ensures a smooth browsing experience.",
     image: Gpt3,
     tags: ["React JS", "Sass"],
     github: "https://github.com/anshulgupta48/Anon-Ecommerce-Website",
@@ -147,7 +192,8 @@ export const projects = [
   },
   {
     title: "Anon Ecommerce Website",
-    description: "Designed and Developed a Real-World Ecommerce Website named 'Anon' that offers a wide range of products and features to enhance your online shopping experience. It Has 6 Sections - Categories, Deals of the Day, Recommended For You, Electronics, Our Founder & Services, Blogs Section. 'Anon' is a fully responsive website developed using HTML, CSS and JavaScript. Regardless of the device you use, be it a desktop computer, tabley, or smartphone, 'Anon' ensures a smooth browsing experience.",
+    description:
+      "Designed and Developed a Real-World Ecommerce Website named 'Anon' that offers a wide range of products and features to enhance your online shopping experience. It Has 6 Sections - Categories, Deals of the Day, Recommended For You, Electronics, Our Founder & Services, Blogs Section. 'Anon' is a fully responsive website developed using HTML, CSS and JavaScript. Regardless of the device you use, be it a desktop computer, tabley, or smartphone, 'Anon' ensures a smooth browsing experience.",
     image: Anon,
     tags: ["HTML", "CSS", "JavaScript"],
     github: "https://github.com/anshulgupta48/Anon-Ecommerce-Website",
@@ -155,7 +201,8 @@ export const projects = [
   },
   {
     title: "Disney+ Hotstar Clone",
-    description: "I Have Created a 'Disney Plus Hotstar' Clone. It is a fully responsive website developed using HTML, CSS and JavaScript. It Has 3 Sections - Recommended For You, Popular Shows, New Releases from where Users Can Navigate to their Favourite Shows. Regardless of the device you use, be it a desktop computer, tabley, or smartphone, it ensures a smooth browsing experience.",
+    description:
+      "I Have Created a 'Disney Plus Hotstar' Clone. It is a fully responsive website developed using HTML, CSS and JavaScript. It Has 3 Sections - Recommended For You, Popular Shows, New Releases from where Users Can Navigate to their Favourite Shows. Regardless of the device you use, be it a desktop computer, tabley, or smartphone, it ensures a smooth browsing experience.",
     image: Hotstar,
     tags: ["HTML", "CSS", "JavaScript"],
     github: "https://github.com/anshulgupta48/Disney-Plus-Hotstar-Clone",
@@ -163,7 +210,8 @@ export const projects = [
   },
   {
     title: "The Garden Cafe Website",
-    description: "Designed and Developed a Real-World Cafe Website named 'The Garden Cafe' that promises an immersive and enchanting online experience. 'The Garden Cafe' is a fully responsive website developed using HTML, CSS and JavaScript. Regardless of the device you use, be it a desktop computer, tabley, or smartphone, 'The Garden Cafe' ensures a smooth browsing experience.",
+    description:
+      "Designed and Developed a Real-World Cafe Website named 'The Garden Cafe' that promises an immersive and enchanting online experience. 'The Garden Cafe' is a fully responsive website developed using HTML, CSS and JavaScript. Regardless of the device you use, be it a desktop computer, tabley, or smartphone, 'The Garden Cafe' ensures a smooth browsing experience.",
     image: GardenCafe,
     tags: ["HTML", "CSS", "JavaScript"],
     github: "https://github.com/anshulgupta48/The-Garden-Cafe-Website",
@@ -171,10 +219,11 @@ export const projects = [
   },
   {
     title: "Fithub Gym Website",
-    description: "'Fithub' is a Real-World Gym Website Which offers a comprehensive fitness experience for users. 'Fithub' is a fully responsive website developed using HTML, CSS and JavaScript. It Has 4 dynamic pages including Home, About, Classes, and Contact. Regardless of the device you use, be it a desktop computer, tabley, or smartphone, 'Fithub' ensures a smooth browsing experience.",
+    description:
+      "'Fithub' is a Real-World Gym Website Which offers a comprehensive fitness experience for users. 'Fithub' is a fully responsive website developed using HTML, CSS and JavaScript. It Has 4 dynamic pages including Home, About, Classes, and Contact. Regardless of the device you use, be it a desktop computer, tabley, or smartphone, 'Fithub' ensures a smooth browsing experience.",
     image: Fithub,
     tags: ["HTML", "CSS", "JavaScript"],
     github: "https://github.com/anshulgupta48/Fithub-Gym-Website",
     link: "https://anshulgupta48.github.io/Fithub-Gym-Website",
-  }
+  },
 ];
